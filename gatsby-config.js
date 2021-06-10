0f8c51b65aa5c7c347823aab3db16556dfd16c86
Chris <christophersgillis@gmail.com>
2021-06-09 22:19:48 -0400

    Initial Config

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,20 +2,20 @@ module.exports = {
   siteMetadata: {
     // Site URL for when it goes live
     siteUrl: `https://elastic-meninsky-aa7c74.netlify.app/`,
-    // Your Name
-    name: 'Ryan Fitzgerald',
+    // Your Names
+    name: 'Chris Gillis',
     // Main Site Title
-    title: `Ryan Fitzgerald | Full-Stack Developer`,
+    title: `Chris Gillis | Full-Stack Developer`,
     // Description that goes under your name in main bio
-    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit, ipsum.`,
+    description: `Full-Stack Developer based in Boston, Massachusetts.`,
     // Optional: Twitter account handle
-    author: `@rfitzio`,
+    author: `@Chris_S_Gillis`,
     // Optional: Github account URL
-    github: `https://github.com/ryanfitzgerald`,
+    github: `https://github.com/christophersgillis`,
     // Optional: LinkedIn account URL
-    linkedin: `https://www.linkedin.com/in/ryanafitzgerald/`,
+    linkedin: `https://www.linkedin.com/in/christophersgillis/`,
     // Content of the About Me section
-    about: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus perferendis porro cumque ea error ab voluptatem. Temporibus adipisci exercitationem similique itaque quibusdam laudantium, qui molestiae quas, aut amet animi id.`,
+    about: `I'm a full stack software engineer constantly seeking to innovate and improve the products I work on. My background in game design makes me particularly cognizant of the importance of the User Experience to good software. I have a need to know how things work which is not only the reason I got into software but also makes me effective at debugging issues efficiently.`,
     // Optional: List your projects, they must have `name` and `description`. `link` is optional.
     projects: [
       {
@@ -40,19 +40,19 @@ module.exports = {
     // Optional: List your experience, they must have `name` and `description`. `link` is optional.
     experience: [
       {
-        name: 'Acme Corp',
-        description: 'Full-Stack Developer, February 2020 - Present',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
+        name: 'Aras Corp',
+        description: 'Senior Software Engineer (Aras Labs), December 2020 - Present',
+        link: 'https://github.com/ArasLabs',
       },
       {
-        name: 'Globex Corp',
-        description: 'Full-Stack Developer, December 2017 - February 2020',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
+        name: 'Aras Corp',
+        description: 'Software Engineer (Aras Labs), Sep 2017 - December 2020',
+        link: 'https://github.com/ArasLabs',
       },
       {
-        name: 'Hooli',
-        description: 'Full-Stack Developer, May 2015 - December 2017',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
+        name: 'Aras Corp',
+        description: 'Software Solutions Engineer, June 2016 - August 2017',
+        link: 'https://www.aras.com',
       },
     ],
     // Optional: List your skills, they must have `name` and `description`.
@@ -60,16 +60,16 @@ module.exports = {
       {
         name: 'Languages & Frameworks',
         description:
-          'JavaScript (ES6+), Golang, Node.js, Express.js, React, Ruby on Rails, PHP',
+          'C#, .NET, JavaScript (ES6+), HTML5/CSS, Vue.JS, C++, C, Java',
       },
       {
         name: 'Databases',
-        description: 'MongoDB, PostreSQL, MySQL',
+        description: 'Microsoft SQL Server',
       },
       {
         name: 'Other',
         description:
-          'Docker, Amazon Web Services (AWS), CI / CD, Microservices, API design, Agile / Scrum',
+          'Git/GitKraken, IIS, Visual Studio, Aras Innovator, Unity, Unreal Engine 4',
       },
     ],
   },
